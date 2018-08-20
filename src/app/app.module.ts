dc60bf521292c10b30e3319c330b44c1bbfb5b9e
Andre Ramos da Silva <andre-ramos1994@hotmail.com>
2018-08-20 14:12:14 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { FeedPageModule } from '../pages/feed/feed.module';
+import { IntoPageModule } from '../pages/into/into.module';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    FeedPageModule,
+    IntoPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
